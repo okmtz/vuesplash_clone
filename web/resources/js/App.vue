a5880c5f5bb8069ded2f08a75db80b33283b7313
souken-b <yuto430scuderia@gmail.com>
2020-11-03 17:17:18 +0900

    create auth page

--- web/resources/js/App.vue
+++ web/resources/js/App.vue
@@ -1,9 +1,27 @@
 <template>
     <div>
-    <main>
-        <div class="container">
-            <RouterView />
-        </div>
-    </main>
+        <header>
+            <Navbar />
+        </header>
+        <main>
+            <div class="container">
+                <RouterView />
+            </div>
+        </main>
+        <footer>
+            <Footer />
+        </footer>
     </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
